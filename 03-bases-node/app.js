4e92d88afdeb87bb9fe0b9fabcee88922c8d97b7
LuisRdz224 <180563@upslp.edu.mx>
2023-01-09 20:51:32 -0600

    cap 34: Instalacion de Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,15 @@
 const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 //imprimir la tabla del 5
 console.clear();
 
 console.log(process.argv);
-const [,,arg3 = 'base=5'] = process.argv;
-const [,base=5] = arg3.split('=');
-console.log(base);
+console.log(argv);
+console.log(argv.base);
 
 
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo))
-    .catch(err => console.log(err));
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(nombreArchivo))
+//     .catch(err => console.log(err));
