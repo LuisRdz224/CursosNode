8d92aa982ae33b11641cc3a06602d45051a2b610
LuisRdz224 <180563@upslp.edu.mx>
2023-01-10 19:44:18 -0600

    cap 36: Yargs independientes

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,23 +1,5 @@
-const { boolean } = require('yargs');
 const {crearArchivo} = require('./helpers/multiplicar');
-const argv = require('yargs')
-                .option('b',{
-                    alias: 'base',
-                    type: 'number',
-                    demandOption: true
-                })
-                .check((argv,options)=>{
-                    if(isNaN(argv.b)){
-                        throw "La base tiene que ser un numero";
-                    }
-                    return true;
-                })
-                .option('l',{
-                    alias:'listar',
-                    type: 'boolean',
-                    default: false
-                })
-                .argv;
+const argv = require('./config/yargs')
 
 //imprimir la tabla del 5
 console.clear();
