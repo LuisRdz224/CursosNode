0cfa35b13791da6efb218df917ffe3860cf25cf7
LuisRdz224 <180563@upslp.edu.mx>
2023-01-17 18:37:41 -0600

    cap 49: Menu con inquirer

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,6 +1,8 @@
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer');
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 
 console.clear();
 
@@ -9,9 +11,8 @@ const main = async() =>{
 
     let opt ='';
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({opt});
-        if(opt!=='0')await pausa();
     } while (opt!=='0');
 
 }
---
+++ 04-tareas-hacer/helpers/inquirer.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer')
+require('colors')
+
+const menuOpts = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: ['opt1', 'opt2', 'opt3']
+    }
+]
+
+const inquirerMenu = async () => {
+    console.clear()
+    console.log('======================='.green)
+    console.log('Seleccione una opcion'.yellow)
+    console.log('======================='.green)
+
+    const opt = await inquirer.prompt(menuOpts)
+    return opt
+}
+
+module.exports = {
+    inquirerMenu
+}
