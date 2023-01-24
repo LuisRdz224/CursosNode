212b75ee47558f275b318bb9b9a3d8d717560c50
LuisRdz224 <180563@upslp.edu.mx>
2023-01-23 18:55:27 -0600

    cap 51: Tareas con uuid

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,18 +1,25 @@
 require('colors');
 
 const { inquirerMenu,  pausa} = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 // const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 
-console.clear();
 
 const main = async() =>{
     console.log("Hola Mundo");
 
     let opt ='';
     do {
-        opt = await inquirerMenu();
-        console.log({opt});
+        // opt = await inquirerMenu();
+        // console.log({opt});
+
+        const tareas = new Tareas();
+        const tarea = new Tarea('Comprar comida');
+
+        tareas._listado[tarea.id] = tarea;
+        console.log(tareas);
 
         await pausa();
     } while (opt!=='0');
