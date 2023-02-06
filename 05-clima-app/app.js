aeee54f67ce289c425a02cb1d4a5eba7bb858957
LuisRdz224 <180563@upslp.edu.mx>
2023-02-06 15:45:50 -0600

    cap 69: Modelo de menu

--- 05-clima-app/app.js
+++ 05-clima-app/app.js
@@ -1,13 +1,43 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const { Busquedas } = require("./models/busquedas");
 
 const main = async() =>{
+    const busquedas = new Busquedas();
     let opt = '';
 
     do {
         opt = await inquirerMenu();
         console.log(opt);
-        
-        await pausa();
+        switch (opt) {
+            case 1:
+                //Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+
+                //Buscar los lugares
+
+                // Seleccionar el lugar
+
+                // Clima
+
+                // Mostrar resultados
+                // const ciudades = await busquedas.ciudad();
+                // console.log(ciudades);
+
+                console.log('\n Informacion de la ciudad\n'.green);
+                console.log('Ciudad:',);
+                console.log('Lat:',);
+                console.log('Lng:',);
+                console.log('Temperatura:',);
+                console.log('Minima:',);
+                console.log('Maxima:',);
+                break;
+            case 2:
+                const historial = busquedas.historial;
+                console.log(historial);
+                break;
+        }
+        if( opt !==0 ) await pausa();
     } while (opt!==0);
 }
 main();
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,20 @@
+
+
+class Busquedas{
+    historial = ['Tegucigalpa','Madrid','San Jose'];
+
+    constructor(){
+        // TODO leer DB si existe
+    }
+    async ciudad(lugar =''){
+        // peticion http
+        console.log(lugar);
+
+        return []; //retornar los lugares
+
+    }
+}
+
+module.exports = {
+    Busquedas
+}
