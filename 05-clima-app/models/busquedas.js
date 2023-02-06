81ee5c6ad512dc694e16a5ecd6a8e1803bd019ae
LuisRdz224 <180563@upslp.edu.mx>
2023-02-06 17:30:00 -0600

    cap 73: Conexion a endpoint de Mapbox

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -11,7 +11,7 @@ class Busquedas{
         try {
             // peticion http
             console.log('Ciudad',lugar);
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Ottawa.json?proximity=ip&language=es&access_token=pk.eyJ1Ijoic2NydWIyMjQiLCJhIjoiY2xkdGRoNXg0MjBtZzN3cWU2cGZ5dzF0ZyJ9.wLAPKsDEmuGPgsgpUIG28w');
             console.log(resp.data);
         } catch (error) {
             return [];
