15071cc954b6edaf6cf20ce038c9f91e1b783834
LuisRdz224 <180563@upslp.edu.mx>
2023-02-06 17:40:01 -0600

    cap 74: Crear instancias de axios

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -6,18 +6,32 @@ class Busquedas{
     constructor(){
         // TODO leer DB si existe
     }
+
+    get paramsMapbox(){
+        return {
+            'access_token': 'pk.eyJ1Ijoic2NydWIyMjQiLCJhIjoiY2xkdGRoNXg0MjBtZzN3cWU2cGZ5dzF0ZyJ9.wLAPKsDEmuGPgsgpUIG28w',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
     async ciudad(lugar =''){
 
         try {
             // peticion http
-            console.log('Ciudad',lugar);
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Ottawa.json?proximity=ip&language=es&access_token=pk.eyJ1Ijoic2NydWIyMjQiLCJhIjoiY2xkdGRoNXg0MjBtZzN3cWU2cGZ5dzF0ZyJ9.wLAPKsDEmuGPgsgpUIG28w');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+
+            });
+
+            const resp = await instance.get();
             console.log(resp.data);
+
+            return []; //retornar los lugares
+
         } catch (error) {
             return [];
         }
-        return []; //retornar los lugares
-
     }
 }
 
