e28e3cdbaed38b82a28b68628b21c2090f7af59e
LuisRdz224 <luisrdzh224@gmail.com>
2023-02-10 21:53:34 -0600

    cap 90: Ejemplos de url sin extension .html, etc

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -5,8 +5,11 @@ const port = 8080;
 //Servir contenido estatico
 app.use(express.static('public'));
 
-app.get('/holamundo', (req, res) => {
-    res.send('Hello World en su respectiva ruta')
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
+})
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
 })
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html')
