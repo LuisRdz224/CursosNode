4b58ce7f13db0831babd846903a9b958fcf0d641
LuisRdz224 <luisrdzh224@gmail.com>
2023-02-13 20:40:24 -0600

    cap 110: Uso de Controllers y Routes

---
+++ 07-restserver/controllers/usuarios.js
@@ -0,0 +1,38 @@
+const { response } = require('express')
+
+
+const usuariosGet = (req, res) => {
+    res.json({
+        msg: 'get API - Controlador'
+    })
+}
+
+const usuariosPost = (req, res) => {
+    res.status(201).json({
+        msg: 'post API - Controlador'
+    })
+}
+const usuariosPut = (req, res) => {
+    res.status(400).json({
+        msg: 'put API - Controlador'
+    })
+}
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - Controlador'
+    })
+}
+const usuariosPatch = (req, res) => {
+    res.json({
+        msg: 'patch API - Controlador'
+    })
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete,
+    usuariosPatch,
+}
